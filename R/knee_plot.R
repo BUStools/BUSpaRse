81b964c8411a640c985a9bd9ab043e718c43ca28
Lambda Moses <dlu2@caltech.edu>
2020-04-22 15:54:16 -0700

    Put documentations of all knee plot related functions in one page

--- R/knee_plot.R
+++ R/knee_plot.R
@@ -1,12 +1,48 @@
+#' @rdname knee_plot
+#' @param mat Gene count matrix, a dgCMatrix.
+#' @return `get_knee_df` returns a tibble with two columns: \code{total} for 
+#' total UMI counts for each barcode, and \code{rank} for rank of the total 
+#' counts, with number 1 for the barcode with the most counts.
+#' @export
+#' 
+get_knee_df <- function(mat) {
+  tibble(total = colSums(mat),
+         rank = row_number(desc(total))) %>%
+    distinct() %>%
+    filter(total > 0) %>% 
+    arrange(rank)
+}
+
+#' @rdname knee_plot
+#' @param df The data frame from \code{\link{get_knee_df}}.
+#' @param lower Minimum total UMI counts for barcode for it to be considered
+#' when calculating the inflection point; this helps to avoid the noisy part of
+#' the curve for barcodes with very few counts.
+#' @return `get_inflection` returns a \code{numeric(1)} for the total UMI count 
+#' at the inflection point.
+#' @note Code in part adapted from \code{barcodeRanks} from \code{DropetUtils}.
+#' @export
+#' @importFrom dplyr transmute
+#' 
+get_inflection <- function(df, lower = 100) {
+  df_fit <- df %>% 
+    filter(total > lower) %>% 
+    transmute(log_total = log10(total),
+              log_rank = log10(rank))
+  d1n <- diff(df_fit$log_total)/diff(df_fit$log_rank)
+  right.edge <- which.min(d1n)
+  10^(df_fit$log_total[right.edge])
+}
+
 #' Plot the transposed knee plot and inflection point
 #' 
-#' This function plots a transposed knee plot, showing the inflection point and
+#' Plot a transposed knee plot, showing the inflection point and
 #' the number of remaining cells after inflection point filtering. It's
 #' transposed since it's more generalizable to multi-modal data.
 #' 
 #' @param df The data frame from \code{\link{get_knee_df}}.
 #' @param inflection Output of \code{\link{get_inflection}}.
-#' @return A \code{ggplot2} object.
+#' @return `knee_plot` returns a \code{ggplot2} object.
 #' @export
 #' @importFrom ggplot2 ggplot aes geom_path geom_vline geom_hline 
 #' scale_x_log10 scale_y_log10 labs annotation_logticks geom_text
@@ -27,8 +63,10 @@ knee_plot <- function(df, inflection) {
                   rank_cutoff = max(df$rank[df$total > inflection]))
   ggplot(df, aes(total, rank)) +
     geom_path() +
-    geom_vline(aes(xintercept = inflection), data = annot, linetype = 2, color = "gray40") +
-    geom_hline(aes(yintercept = rank_cutoff), data = annot, linetype = 2, color = "gray40") +
+    geom_vline(aes(xintercept = inflection), data = annot, linetype = 2, 
+               color = "gray40") +
+    geom_hline(aes(yintercept = rank_cutoff), data = annot, linetype = 2, 
+               color = "gray40") +
     geom_text(aes(inflection, rank_cutoff, 
                   label = paste(rank_cutoff, "'cells'")),
               data = annot, vjust = 1) +
@@ -36,41 +74,4 @@ knee_plot <- function(df, inflection) {
     scale_y_log10() +
     labs(y = "Rank", x = "Total UMIs") +
     annotation_logticks()
-}
-
-#' @rdname knee_plot
-#' @param mat Gene count matrix, a dgCMatrix.
-#' @return A tibble with two columns: \code{total} for total UMI counts for 
-#' each barcode, and \code{rank} for rank of the total counts, with number 1
-#' for the barcode with the most counts.
-#' @export
-#' 
-get_knee_df <- function(mat) {
-  tibble(total = colSums(mat),
-         rank = row_number(desc(total))) %>%
-    distinct() %>%
-    filter(total > 0) %>% 
-    arrange(rank)
-}
-
-#' Compute inflection point of knee plot
-#'
-#'
-#' @param df The data frame from \code{\link{get_knee_df}}.
-#' @param lower Minimum total UMI counts for barcode for it to be considered
-#' when calculating the inflection point; this helps to avoid the noisy part of
-#' the curve for barcodes with very few counts.
-#' @return A \code{numeric(1)} for the total UMI count at the inflection point.
-#' @note Code in part adapted from \code{barcodeRanks} from \code{DropetUtils}.
-#' @export
-#' @importFrom dplyr transmute
-#' 
-get_inflection <- function(df, lower = 100) {
-  df_fit <- df %>% 
-    filter(total > lower) %>% 
-    transmute(log_total = log10(total),
-              log_rank = log10(rank))
-  d1n <- diff(df_fit$log_total)/diff(df_fit$log_rank)
-  right.edge <- which.min(d1n)
-  10^(df_fit$log_total[right.edge])
 }
